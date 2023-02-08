42c3bb2ee2b3504f88f0f55fee44f2fe3f108a03
SonyMC <mailsonymathew@gmail.com>
2023-02-08 15:49:48 +0530

    Feb08_2023

--- ApacheKafkaStreams/Project/USER-EVENT-ENRICHER/src/main/java/com/mailsonymathew/kafka/streams/UserEventEnricherApp.java
+++ ApacheKafkaStreams/Project/USER-EVENT-ENRICHER/src/main/java/com/mailsonymathew/kafka/streams/UserEventEnricherApp.java
@@ -13,6 +13,9 @@ import java.util.Properties;
 public class UserEventEnricherApp {
     public static void main(String[] args) {
 
+        /*
+        - Create Properties
+         */
         Properties config = new Properties();
         config.put(StreamsConfig.APPLICATION_ID_CONFIG, "user-event-enricher-app");
         config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
@@ -20,22 +23,26 @@ public class UserEventEnricherApp {
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
+        // Stream Builder
         StreamsBuilder builder = new StreamsBuilder();
 
-        // we get a global table out of Kafka. This table will be replicated on each Kafka Streams application
+        // Global Table: we get a global table out of Kafka. This table will be replicated on each Kafka Streams application
         // the key of our globalKTable is the user ID
+        // Note: There is no need to specify the serdes as we have already done tht in the stream properties setup
         GlobalKTable<String, String> usersGlobalTable = builder.globalTable("user-table");
 
-        // we get a stream of user purchases
+        // We get a stream of user purchases
         KStream<String, String> userPurchases = builder.stream("user-purchases");
 
-        // we want to enrich that stream
+        // We want to enrich the userPurchases Steam wih user information from the global table.
+        // Create an Inner join ( i.e. data will exist on both LHS and RHS)
         KStream<String, String> userPurchasesEnrichedJoin =
-                userPurchases.join(usersGlobalTable,
+                userPurchases.join(usersGlobalTable,  // Inner  Join
                         (key, value) -> key, /* map from the (key, value) of this stream to the key of the GlobalKTable */
-                        (userPurchase, userInfo) -> "Purchase=" + userPurchase + ",UserInfo=[" + userInfo + "]"
+                        (userPurchase, userInfo) -> "Purchase=" + userPurchase + ",UserInfo=[" + userInfo + "]"  // userPurchase is the user purchase stream while the userInfo is the data from the global table
                 );
 
+        // Write the enriched inner join KStream to the topic 'user-purchases-enriched-inner-join'
         userPurchasesEnrichedJoin.to("user-purchases-enriched-inner-join");
 
         // we want to enrich that stream using a Left Join
@@ -52,9 +59,11 @@ public class UserEventEnricherApp {
                         }
                 );
 
+        // Write the enriched left join KStream to the topic 'user-purchases-enriched-left-join'
         userPurchasesEnrichedLeftJoin.to("user-purchases-enriched-left-join");
 
 
+        // Build Kafka Streams application and start its
         KafkaStreams streams = new KafkaStreams(builder.build(), config);
         streams.cleanUp(); // only do this in dev - not in prod
         streams.start();
